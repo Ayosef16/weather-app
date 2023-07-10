6d4abd4bbf7c90f3dde7f9319a43c3551cf50280
Ayosef <syrivera@uc.cl>
2023-07-10 12:57:56 -0400

    Add header, footer and fetch for weather data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "normalize.css";
+import "./style.css";
+
+async function getCurrentWeather() {
+  const weatherHeader = await fetch(
+    "https://api.weatherapi.com/v1/forecast.json?key=62db037f513a4bdcba5133348230607&q=miami&days=3",
+    {
+      mode: "cors",
+    }
+  );
+  console.log(weatherHeader);
+  const weather = await weatherHeader.json();
+  console.log(weather);
+  return weather;
+}
+
+async function displayCurrentWeather() {
+  const test = document.querySelector("#test");
+  const weather = await getCurrentWeather();
+  test.textContent = weather.current.condition.text;
+}
+
+displayCurrentWeather();
